e874f9299b34b6262416f5f296307f63386faa1f
Christian Martiny <christian.martiny@outlook.com>
2023-10-11 11:39:28 +0200

    SOUSTRACTION : modification de base.py

---
+++ base.py
@@ -0,0 +1,33 @@
+# from addition import addition as addition
+from soustraction import soustraction as soustraction
+# from multiplication import multiplication as multiplication
+# from division import division as division
+
+def input_nombres():
+    num1 = input("Nombre 1 ?")
+    num2 = input("Nombre 2 ?")
+    return [num1, num2]
+
+def affichage(operateur, num1, num2, resultat) -> str:
+    print(f"{num1} {operateur} {num2} = {resultat}")
+
+operation = ""
+
+while operation != "Q":
+    print("")
+    
+    operation = input("Quel opération voulez-vous faire ?\n'+' => addition\n'-' => soustraction\n'*' => multiplication\n'/' => division\n'Q' => quitter\n\n")
+
+    if operation == "+":
+        print("")
+    elif operation == "-":
+        nombres = input_nombres()
+        affichage(operation, nombres[0], nombres[1], soustraction(nombres[0], nombres[1]))
+    elif operation == "*":
+        print("")
+    elif operation == "/":
+        print("")
+    elif operation == "Q":
+        print("Bye bye !")
+    else:
+        print("Cette opération n'est pas conforme !")
