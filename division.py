c9a23862acf25cd3180f4a9db4585fb3c39e7431
Cecile HURIAUX <cecile.huriaux@gmail.com>
2023-10-11 11:39:53 +0200

    codage de la fonction pour la division

---
+++ division.py
@@ -0,0 +1,27 @@
+# print("Bienvenue dans les divisions")
+# while True: 
+#     nombre_div_1 = input("Entrez le premier nombre : ")
+#     nombre_div_2 = input("diviser par : ")
+
+#     if nombre_div_1.isdigit() and nombre_div_2.isdigit():
+#         nombre_div_1 = int(nombre_div_1)
+#         nombre_div_2 = int(nombre_div_2)
+#         resultat = nombre_div_1 / nombre_div_2
+#         print(f"Le résultat est = {resultat}")
+#         break
+#     else:
+#         print("Veuillez entrez deux nombres valides")
+
+
+def divide(nombre_div_1, nombre_div_2):
+#     error = "Veuillez entrez deux nombres valides"
+
+    return nombre_div_1 / nombre_div_2
+#         if nombre_div_1.isdigit() and nombre_div_2.isdigit():
+#             return resultat
+#         else:
+#             return error
+    
+nombre_div_1 = 0
+nombre_div_2 = 0
+print(divide(nombre_div_1, nombre_div_2))
