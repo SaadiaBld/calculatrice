94bdb9898f8c76b570cfb12b5000bd67ccb28793
Christian Martiny <christian.martiny@outlook.com>
2023-10-11 10:46:37 +0200

    SOUSTRACTION : code robuste

--- addition.py
+++ addition.py
@@ -1,2 +1,8 @@
 def addition(num1: float, num2: float) -> float:
-    return num1 - num2
+    strNum1 = str(num1)
+    strNum2 = str(num2)
+
+    if not strNum1.isnumeric() or not strNum2.isnumeric():
+        return "STR ERROR"
+    
+    return float(strNum1) - float(strNum2)
